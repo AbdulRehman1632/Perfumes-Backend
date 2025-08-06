6bdada60536cba5282aab9a857862c75ca6b257f
AbdulRehman1632 <a.rehman1632@gmail.com>
2025-08-06 15:44:58 +0500

    Adding get

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: ['https://perfumes-frontend-one.vercel.ap'],
+  origin: ['https://perfumes-frontend-one.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true}))
 
